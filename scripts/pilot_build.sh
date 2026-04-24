2b5e230645dee98d0428b9f56f11e2f7e5d08bd6
SeaCannon1122 <coroc@buzzmail.eu>
2026-04-24 20:00:44 +0200

    pilot fix

--- scripts/pilot_build.sh
+++ scripts/pilot_build.sh
@@ -1,1 +1,19 @@
-docker compose -f docker-compose-files.local.yaml build inverse_kinematics rsp twist_mux nav2 slam_toolbox ohm_slam gazebo rviz
+cmd=(docker compose)
+
+[[ " $@ " =~ " -remote "    ]] \
+  && cmd+=(-f docker-compose.yaml -f docker-compose.remote.yaml)
+
+cmd+=(
+build \
+inverse_kinematics \
+rsp \
+twist_mux \
+nav2 \
+slam_toolbox \
+ohm_slam \
+gazebo \
+rviz
+)
+
+
+"${cmd[@]}"
